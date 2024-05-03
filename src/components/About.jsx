f7a9431ddc349e9b45547f3663e38fc27ac12585
Ahmad Searcy <ahmadsearcy7@gmail.com>
2024-05-03 06:20:53 -0500

    Experience complete

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,19 +5,20 @@ const About = ()=>{
 
     return(
         <>
-        <section className='p-10' id="About">
-            <div className='flex justify-center' id="img-and-text">
-                <img className='w-1/5 rounded-full ' src="Portfolio-picture.jpg" alt="image of Ahmad " />
-                <div id="img-text">
-                    <p className='text-1xl'>Hello, I'm</p>
-                    <h2 className='font-bold text-4xl'>Ahmad Searcy</h2>
-                    <h3 className='font-bold text-2xl'>Frontend Engineer</h3>
-                    <div id="download-buttons">
-                    
+        <section className='p-10 min-h-screen' id="About">
+            <div className='flex justify-center gap-20 mt-40' id="img-and-text">
+                <img className='w-1/4 rounded-full ' src="Portfolio-picture.jpg" alt="image of Ahmad " />
+                <div className='flex flex-col gap-10 text-center mt-40' id="img-text">
+                    <div className='flex flex-col gap-3' id="about-text">
+                        <p className='text-1xl font-bold'>Hello, I'm</p>
+                        <h2 className='font-bold text-5xl'>Ahmad Searcy</h2>
+                        <h3 className='font-bold text-3xl text-gray-500'>Frontend Engineer</h3>
+                        <div id="download-buttons">
                     </div>
-                    <div id="linkedin-github">
-                    <a href="https://github.com/as31212"><i className="fa-brands fa-github"></i></a>
-                    <a href="https://www.linkedin.com/in/ahmad-searcy/"><i className="fa-brands fa-linkedin"></i></a>
+                    </div>
+                    <div className='flex justify-center gap-5' id="linkedin-github">
+                    <a target='_blank' href="https://github.com/as31212"><i className="fa-brands fa-github text-4xl"></i></a>
+                    <a target='_blank' href="https://www.linkedin.com/in/ahmad-searcy/"><i className="fa-brands fa-linkedin text-4xl"></i></a>
                     </div>
                 </div>
             </div>
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,10 +1,126 @@
+const Experience = () => {
+  return (
+    <>
+      <div className="h-screen min-h-screen mt-5" id="Experience">
+        <p className="text-center">Explore My</p>
+        <h2 className="text-center text-5xl">Experience</h2>
+        <div className="flex flex-wrap justify-center gap-10 p-40" id="frontend-backend">
+          <div
+            className="w-2/5 p-10  rounded-xl  border-2 border-black"
+            id="frontend"
+          >
+            <h2 className="mb-10 text-center text-3xl text-gray-500">
+              Frontend Development
+            </h2>
+            <div className="flex flex-wrap justify-between" id="frontend-skills">
+              <div className="flex flex-wrap flex-col" id="frontend-skills-left">
+                <div className="mb-10 skill flex flex-wrap ">
+                  <i className="fa-solid fa-check mt-1 text-2xl mr-2 ml-2"></i>
+                  <div className="skill-text text-2xl ">
+                    <p className="font-bold">HTML</p>
+                    <p>Experienced</p>
+                  </div>
+                </div>
+                <div className="mb-10 skill flex flex-wrap ">
+                  <i className="fa-solid fa-check mt-1 text-2xl mr-2 ml-2"></i>
+                  <div className="skill-text text-2xl ">
+                    <p className="font-bold">CSS</p>
+                    <p>Experienced</p>
+                  </div>
+                </div>
+                <div className="mb-10 skill flex flex-wrap ">
+                  <i className="fa-solid fa-check mt-1 text-2xl mr-2 ml-2"></i>
+                  <div className="skill-text text-2xl ">
+                    <p className="font-bold">Javascript</p>
+                    <p>Experienced</p>
+                  </div>
+                </div>
+              </div>
+              <div className="flex flex-wrap flex-col" id="frontend-skills-right">
+                <div className="mb-10 skill flex flex-wrap ">
+                  <i className="fa-solid fa-check mt-1 text-2xl mr-2 ml-2"></i>
+                  <div className="skill-text text-2xl ">
+                    <p className="font-bold">React</p>
+                    <p>Experienced</p>
+                  </div>
+                </div>
+                <div className="mb-10 skill flex flex-wrap ">
+                  <i className="fa-solid fa-check mt-1 text-2xl mr-2 ml-2"></i>
+                  <div className="skill-text text-2xl ">
+                    <p className="font-bold">Tailwind</p>
+                    <p>Experienced</p>
+                  </div>
+                </div>
+                <div className="mb-10 skill flex flex-wrap ">
+                  <i className="fa-solid fa-check mt-1 text-2xl mr-2 ml-2"></i>
+                  <div className="skill-text text-2xl ">
+                    <p className="font-bold">Typescript</p>
+                    <p>Intermediate</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
 
-const Experience = ()=>{
-
-    return(
-        <>
-        
-        </>
-    );
-}
+          <div
+            className="w-2/5 p-10  rounded-xl  border-2 border-black"
+            id="backend"
+          >
+            <h2 className="mb-10 text-center text-3xl text-gray-500">
+              Backend Development
+            </h2>
+            <div className="flex flex-wrap justify-between" id="backend-skills">
+              <div className="flex flex-wrap flex-col" id="backend-skills-left">
+                <div className="mb-10 skill flex flex-wrap ">
+                  <i className="fa-solid fa-check mt-1 text-2xl mr-2 ml-2"></i>
+                  <div className="skill-text text-2xl ">
+                    <p className="font-bold">Python</p>
+                    <p>Basic</p>
+                  </div>
+                </div>
+                <div className="mb-10 skill flex "flex-wrap >
+                  <i className="fa-solid fa-check mt-1 text-2xl mr-2 ml-2"></i>
+                  <div className="skill-text text-2xl ">
+                    <p className="font-bold">SQL</p>
+                    <p>Intermediate</p>
+                  </div>
+                </div>
+                <div className="mb-10 skill flex flex-wrap ">
+                  <i className="fa-solid fa-check mt-1 text-2xl mr-2 ml-2"></i>
+                  <div className="skill-text text-2xl ">
+                    <p className="font-bold">NodeJS</p>
+                    <p>Basic</p>
+                  </div>
+                </div>
+              </div>
+              <div className="flex flex-wrap flex-col" id="backend-skills-right">
+                <div className="mb-10 skill flex flex-wrap ">
+                  <i className="fa-solid fa-check mt-1 text-2xl mr-2 ml-2"></i>
+                  <div className="skill-text text-2xl ">
+                    <p className="font-bold">ExpressJS</p>
+                    <p>Basic</p>
+                  </div>
+                </div>
+                <div className="mb-10 skill flex flex-wrap ">
+                  <i className="fa-solid fa-check mt-1 text-2xl mr-2 ml-2"></i>
+                  <div className="skill-text text-2xl ">
+                    <p className="font-bold">MongoDB</p>
+                    <p>Basic</p>
+                  </div>
+                </div>
+                <div className="mb-10 skill flex flex-wrap ">
+                  <i className="fa-solid fa-check mt-1 text-2xl mr-2 ml-2"></i>
+                  <div className="skill-text text-2xl ">
+                    <p className="font-bold">Rest API</p>
+                    <p>Basic</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 export default Experience;
