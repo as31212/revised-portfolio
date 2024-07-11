67666c515208f084de806c575aedbed93d2275cf
Ahmad Searcy <ahmadsearcy7@gmail.com>
2024-07-10 22:27:31 -0500

    animation added to nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,7 @@
 const Nav = ({ navActive, toggleNav }) => {
     return (
         <>
-            <nav id="nav" className={navActive ? 'shadow p-5 flex text-3xl flex-col items-center' : 'shadow p-5 flex text-3xl'}>
+            <nav id="nav" className={navActive ? 'shadow p-5 flex text-3xl flex-col items-center active' : 'shadow p-5 flex text-3xl'}>
                 <h2 className="whitespace-nowrap" id="name">Ahmad Searcy</h2>
                 <button className="w-full" onClick={() => toggleNav()} id="nav-button">
                     <div className="flex flex-col gap-2 show-button">
@@ -11,11 +11,11 @@ const Nav = ({ navActive, toggleNav }) => {
                     </div>
                 </button>
                 <br />
-                <ul id="nav-buttons-toggle" className={navActive ? 'flex flex-col gap-10 justify-end' : 'hidden'}>
-                    <a href="#About"><li>About</li></a>
-                    <a href="#Experience"><li>Experience</li></a>
-                    <a href="#Projects"><li>Projects</li></a>
-                    <a href="#Contact"><li>Contact</li></a>
+                <ul id="nav-buttons-toggle" className={navActive ? 'flex flex-col gap-10 justify-center items-center' : 'hidden'}>
+                    <a onClick={()=>toggleNav()} href="#About"><li>About</li></a>
+                    <a onClick={()=>toggleNav()} href="#Experience"><li>Experience</li></a>
+                    <a onClick={()=>toggleNav()} href="#Projects"><li>Projects</li></a>
+                    <a onClick={()=>toggleNav()} href="#Contact"><li>Contact</li></a>
                 </ul>
                 <ul id="nav-buttons" className='flex gap-10'>
                     <a href="#About"><li>About</li></a>
